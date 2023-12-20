b628a71f5ca7a4a29e933747a377a2a87be79d48
le402n40\uer <linyier1027@gmail.com>
2023-12-20 17:00:50 +0800

    練習3

--- 1220.R
+++ 1220.R
@@ -5,3 +5,9 @@ Result
 summary(Result)
 plot(Data$Height, Data$Weight, pch = 19)
 abline(Result)
+
+Result1 = cor.test(Data[,"Weight"], Data[,"Height"], method = "pearson") # Pearson correlation
+Result1
+
+Result2 = cor.test(Data[,"Weight"], Data[,"Height"], method = "spearman") # Spearman correlation
+Result2
