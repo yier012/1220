6ba05326576ea832145866ca3974af211e9bdb1e
le402n40\uer <linyier1027@gmail.com>
2023-12-20 16:50:41 +0800

    散佈圖

--- 1220.R
+++ 1220.R
@@ -1,2 +1,7 @@
 Data=read.table("Example.txt", header = TRUE) #讀取Example.txt，並存成"Data"這個物件(資料表格式)
 head(Data, 10) #看"Data"這個資料表的前10個row
+Result = lm(Weight~Height, data = Data) #linear regression test
+Result
+summary(Result)
+plot(Data$Height, Data$Weight, pch = 19)
+abline(Result)
